f4027346856e7a90c284ede513992f447b38f526
arily <arily@arily.moe>
2020-01-27 02:37:46 +0900

    addref removeref command to add or remove refers

--- constants/fokabotCommands.py
+++ constants/fokabotCommands.py
@@ -1349,6 +1349,30 @@ def getSpectatorHostUserIDFromChannel(chan):
 	return userID
 
 def multiplayer(fro, chan, message):
+	def mpAddRefer():
+		if len(message) < 2:
+			raise exceptions.invalidArgumentsException("Wrong syntax: !mp addref <user>")
+		_match = glob.matches.matches[getMatchIDFromChannel(chan)]
+		username = message[1].strip()
+		if not username:
+			raise exceptions.invalidArgumentsException("Please provide a username")
+		userID = userUtils.getIDSafe(username)
+		if userID is None:
+			raise exceptions.userNotFoundException("No such user")
+		_match.addRefer(userID)
+		return "added {} to refers".format(username)
+	def mpRemoveRefer():
+		if len(message) < 2:
+			raise exceptions.invalidArgumentsException("Wrong syntax: !mp addref <user>")
+		_match = glob.matches.matches[getMatchIDFromChannel(chan)]
+		username = message[1].strip()
+		if not username:
+			raise exceptions.invalidArgumentsException("Please provide a username")
+		userID = userUtils.getIDSafe(username)
+		if userID is None:
+			raise exceptions.userNotFoundException("No such user")
+		_match.removeRefer(userID)
+		return "removed {} from refers".format(username)
 	def mpMake():
 		if len(message) < 2:
 			raise exceptions.invalidArgumentsException("Wrong syntax: !mp make <name>")
@@ -1674,6 +1698,8 @@ def multiplayer(fro, chan, message):
 
 	try:
 		subcommands = {
+			"addref": mpAddRefer,
+			"rmref": mpRemoveRefer,
 			"make": mpMake,
 			"close": mpClose,
 			"join": mpJoin,
